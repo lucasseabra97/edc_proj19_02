60d0632eb5e58a3ba61292e6fff2f97eb105397c
Enamak <gbmvitor@gmail.com>
2019-12-17 00:53:49 +0000

    Gitignore fix

--- edc_proj19_02/urls.py
+++ edc_proj19_02/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
+    path('', views.landing, name="landing"),
     path('admin/', admin.site.urls),
 ]
