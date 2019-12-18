bfd026558dec2c77ef6d78c30a32d99531eb9bc6
Enamak <gbmvitor@gmail.com>
2019-12-18 23:46:59 +0000

    Ganda tabela

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ import json
 import requests
 from s4api.graphdb_api import GraphDBApi
 from s4api.swagger import ApiClient
+import math
 
 endpoint = "http://localhost:7200"
 repo_name = "edc_2019"
@@ -10,41 +11,86 @@ client = ApiClient(endpoint=endpoint)
 accessor = GraphDBApi(client)
 
 # Create your views here.
+def isInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 def landing(request):
+
+    limit = 20
+    page = int(request.GET.get('page')) if request.GET.get('page') else 1
+    order = request.GET.get('order') or 'name'
 
     query = """
         PREFIX country:<http://edc_2019.org/country/>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-        SELECT *
+        SELECT  
+        *
+        WHERE
         {
             ?country rdf:type country:.
-            ?country ?key ?value
-        }
+        } 
     """
 
     payload_query = {"query": query}
+    size = len(json.loads(accessor.sparql_select(body=payload_query, repo_name=repo_name))['results']['bindings'])
+
+    query = """
+        PREFIX country:<http://edc_2019.org/country/>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX pred: <http://edc_2019.org/pred/>
+
+        SELECT  
+        ?country ?plate 
+        ?flag ?name 
+        ?localtime ?population ?life 
+        ?location ?capital
+        ?inflation ?area
+        ?currency ?pib 
+        WHERE
+        {{
+         ?country rdf:type country:.
+            OPTIONAL {{ ?country rdf:type country:. }}
+            OPTIONAL {{ ?country pred:flag ?flag. }}
+            OPTIONAL {{ ?country pred:name ?name. }}
+            OPTIONAL {{ ?country pred:plate ?plate. }}
+            OPTIONAL {{ ?country pred:currency ?currency. }}
+            OPTIONAL {{ ?country pred:pop ?population. }}
+            OPTIONAL {{ ?country pred:area ?area. }}
+            OPTIONAL {{ ?country pred:inflation ?inflation. }}
+            OPTIONAL {{ ?country pred:pib ?pib. }}
+        }}
+        ORDER BY {2}
+        OFFSET {0}
+        LIMIT {1}
+    """.format((page - 1) * limit, limit, 'DESC(xsd:float(?{}))'.format(order) if (order in ['area', 'population', 'inflation', 'pib']) else '?' + order)
+
+    payload_query = {"query": query}
     res = json.loads(accessor.sparql_select(body=payload_query, repo_name=repo_name))['results']['bindings']
-    
-    countries = {}
 
-    for d in res:
-        tmp = d['country']['value'].replace('http://edc_2019.org/country/', '')
-        if tmp not in countries:
-            countries[tmp] = {}
-        countries[tmp][d['key']['value'].replace('http://edc_2019.org/pred/', '').replace('http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'type')] = d['value']['value']
+    maxPage = math.ceil(size / limit)
 
+    pages = [i+1 for i in range(maxPage)]
+    left = [i for i in pages if page - i < 4 and i < page]
+    right = [i for i in pages if i - page < 4 and i > page]
 
-    tmp = list(map(lambda x: {
-        'id': x,
-        **countries[x]
-    },list(countries)))
-
-    #print(tmp)
+    print(left, right)
 
     return render(request, 'landing.html', {
-        'tmp': tmp,
+        'table_order': ['plate', 'name', 'currency', 'population', 'area', 'inflation', 'pib'],
+        'countries': res,
+        'size': size,
+        'left': left,
+        'right': right,
+        'page': page,
+        'maxPage': maxPage,
+        'showMax': (maxPage - page) >= 4,
+        'showMin': page >= 4,
+        'limit': limit,
     })
 
 
